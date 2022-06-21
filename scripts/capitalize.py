de3220f522b8ebe75ed879905edb7a2261e4ba76
Oleks <oleks@oleks.info>
2022-06-21 22:54:33 +0200

    Capitalize words, sentences, and letters proper
    Include the script to do so automatically. Note, some changes induced
    by the script will not be committed (as they are foul), and some
    changes will be committed in a subsequent commit.

---
+++ scripts/capitalize.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import csv
+import string
+import tempfile
+import os
+
+def process_csv(out_file, in_file_path):
+    with open(in_file_path, 'r', encoding='utf-8') as in_file:
+        reader = csv.reader(in_file)
+        writer = csv.writer(
+            out_file,
+            delimiter=',',
+            quoting=csv.QUOTE_ALL,
+            lineterminator='\n')
+        writer.writerow(next(reader))
+        for row in reader:
+            if string.capwords(row[1], ' ') == row[1]:
+                row[2] = string.capwords(row[2], ' ')
+            elif row[1].capitalize() == row[1]:
+                row[2] = row[2].capitalize()
+            elif row[1].upper() == row[1]:
+                row[2] = row[2].upper()
+            writer.writerow(row)
+
+out_file = tempfile.NamedTemporaryFile(
+    mode='w',
+    delete=False,
+    encoding='utf-8',
+    dir='.')
+in_file_path = 'MyoCognitionDictonary.csv'
+try:
+    process_csv(out_file, in_file_path)
+    out_file.close()
+    os.rename(out_file.name, in_file_path)
+except Exception as e:
+    print("fail", str(e))
+    out_file.close()
+    os.remove(out_file.name)
